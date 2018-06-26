f0db80261a0d7bfbd0a0c057eef5bd33a4c36d14
fredkobo <frederick.kobo@gmail.com>
2018-06-26 15:43:52 +0200

    Add logic for google sign-in

---
+++ app/src/main/java/za/co/fredkobo/jotdown/Constants.java
@@ -0,0 +1,10 @@
+package za.co.fredkobo.jotdown;
+
+/**
+ * Created by F5094712 on 2018/06/26.
+ */
+
+public class Constants {
+    public static final String clientId = "596960866099-svnv047edi01cer944lru0vlg1c01qtd.apps.googleusercontent.com";
+    public static final String clientSecret = "fi8l7LxX2Z1n1AioUGPdHyLF";
+}
--- app/src/main/java/za/co/fredkobo/jotdown/LoginActivity.java
+++ app/src/main/java/za/co/fredkobo/jotdown/LoginActivity.java
@@ -8,45 +8,108 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import za.co.fredkobo.jotdown.viewModel.LoginViewModel;
 import za.co.fredkobo.jotdown.viewModel.LoginViewModelInterface;
 
 public class LoginActivity extends AppCompatActivity implements LoginViewInterface {
-
+    private String TAG = LoginActivity.class.getSimpleName();
     // UI references.
-    private EditText emailEditText;
-    private EditText passwordEditText;
+
     private View progressView;
-    private View loginFormScrollView;
-    private Button emailSignInButton;
-    private Button googleSignInButton;
+    private SignInButton googleSignInButton;
     private LoginViewModelInterface viewModel;
+    private int RC_SIGN_IN = 1001;
+
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        viewModel = new LoginViewModel(this);
-        // Set up the login form.
-        emailEditText = (EditText) findViewById(R.id.email_edittext);
-        passwordEditText = (EditText) findViewById(R.id.password_edittext);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        emailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        googleSignInButton = (Button) findViewById(R.id.google_sign_in_button);
+        viewModel = new LoginViewModel(this);
+
+        googleSignInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
         googleSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                viewModel.attemptGoogleSignIn();
+                invokeGoogleSignIn();
+                showProgress(true);
             }
         });
 
-        loginFormScrollView = findViewById(R.id.login_form);
-        progressView = findViewById(R.id.login_progress);
+        progressView = findViewById(R.id.progress_frame);
+    }
+
+    private void invokeGoogleSignIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        userSignedThroughGoogle(account);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            userSignedThroughGoogle(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            userSignedThroughGoogle(null);
+        }
+    }
+
+    private void userSignedThroughGoogle(GoogleSignInAccount account)
+    {
+        showProgress(false);
+        if(account != null){
+            googleSignInButton.setVisibility(View.GONE);
+            this.onSignInSuccess();
+        } else {
+            googleSignInButton.setVisibility(View.VISIBLE);
+        }
+
     }
 
     /**
@@ -60,15 +123,6 @@ public class LoginActivity extends AppCompatActivity implements LoginViewInterfa
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-            loginFormScrollView.setVisibility(show ? View.GONE : View.VISIBLE);
-            loginFormScrollView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    loginFormScrollView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
             progressView.setVisibility(show ? View.VISIBLE : View.GONE);
             progressView.animate().setDuration(shortAnimTime).alpha(
                     show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
@@ -81,7 +135,6 @@ public class LoginActivity extends AppCompatActivity implements LoginViewInterfa
             // The ViewPropertyAnimator APIs are not available, so simply show
             // and hide the relevant UI components.
             progressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            loginFormScrollView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
     }
 
